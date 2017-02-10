02e539b821e8cf055670120f8b2bd7bf25d35113
lovenunu <benjamin@perche.me>
2017-02-10 17:17:04 +0100

    remove schedule call

--- src/app/path/initial-path/schedule-call/schedule-call.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-schedule-call',
-  templateUrl: './schedule-call.component.html',
-  styleUrls: ['./schedule-call.component.scss']
-})
-export class ScheduleCallComponent {
-}
