cf56317c27234b40b915e0deda02be6deac43b49
maksymzav <maksym.zavorotynskyi@gmail.com>
2017-08-30 22:58:38 +0300

    Move login menu to a separate component.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../core/services/user.service';
 import { Router } from '@angular/router';
-import { LocaleIdService } from '../core/services/locale-id.service';
-import { ChannelLanguage } from '../core/entities/channel-language.enum';
 import { toPairs } from 'lodash';
 import { environment } from '../../environments/environment';
 import { FormControl, Validators } from '@angular/forms';
@@ -18,14 +16,9 @@ export class LoginComponent implements OnInit {
     passwordControl = new FormControl('', [Validators.required]);
 
     error = '';
-    localeId: keyof typeof ChannelLanguage;
-    localizations = [];
     appUrl = environment.APP_URL;
 
-    constructor(protected userService: UserService, protected router: Router,
-                protected localeIdService: LocaleIdService) {
-        this.localeId = this.localeIdService.localeId;
-        this.localizations = toPairs(ChannelLanguage).filter(locale => locale[0] !== this.localeId);
+    constructor(protected userService: UserService, protected router: Router) {
 
     }
 
--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { MenuComponent } from './menu.component';
 import { SharedModule } from '../shared/shared.module';
 import { MenuContainerComponent } from './menu-container.component';
+import { UnauthenticatedMenuComponent } from './unauthenticated-menu.component';
 
 @NgModule({
     imports: [
         SharedModule
     ],
-    exports: [MenuComponent, MenuContainerComponent],
-    declarations: [MenuComponent, MenuContainerComponent]
+    exports: [MenuComponent, MenuContainerComponent, UnauthenticatedMenuComponent],
+    declarations: [MenuComponent, MenuContainerComponent, UnauthenticatedMenuComponent]
 })
 export class MenuModule {
 }
