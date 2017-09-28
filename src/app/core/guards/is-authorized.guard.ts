7616928eea1167b6d60ffdd977ef7c9ed5efc393
maksymzav <maksym.zavorotynskyi@gmail.com>
2017-09-28 17:39:14 +0300

    SFWEB-77 Redirect to login if there are no enabled stores.

--- src/app/core/guards/is-authorized.guard.ts
+++ src/app/core/guards/is-authorized.guard.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { WindowRefService } from '../services/window-ref.service';
 import { environment } from '../../../environments/environment';
 import { UserService } from '../services/user.service';
+import { StoreStatus } from '../entities/store-status.enum';
 
 @Injectable()
 export class IsAuthorizedGuard implements CanActivate {
@@ -14,24 +15,41 @@ export class IsAuthorizedGuard implements CanActivate {
         // check Authorization in a storage
         let auth = this.windowRef.nativeWindow.localStorage.getItem('Authorization');
         if (!auth) {
-            this.windowRef.nativeWindow.location.href = environment.BASE_HREF + '/' + environment.LOCALE_ID + '/login';
+            this.redirectToLogin();
             return false;
         }
         return Observable.create(observer => {
             this.userService.fetchAggregatedInfo().subscribe(
-                // activate if no errors
-                () => {
-                    observer.next(true);
-                    observer.complete();
+                userInfo => {
+                    // activate if no errors and the user has at least 1 enabled store
+                    if (this.hasEnabledStore(userInfo._embedded.store, next.queryParams.store)) {
+                        observer.next(true);
+                        observer.complete();
+                    } else {
+                        this.isNotAuthorized(observer)
+                    }
                 },
                 // do not activate and redirect to /login when an error
-                () => {
-                    observer.next(false);
-                    observer.complete();
-                    this.windowRef.nativeWindow.location.href = environment.BASE_HREF + '/' + environment.LOCALE_ID + '/login';
-
-                }
+                () => this.isNotAuthorized(observer)
             );
         });
+    }
+
+    protected redirectToLogin() {
+        this.windowRef.nativeWindow.location.href = environment.BASE_HREF + '/' + environment.LOCALE_ID + '/login';
+    }
+
+    protected hasEnabledStore(store, storeQueryParam) {
+        if (storeQueryParam) {
+            return store.find(s => s.status !== StoreStatus.deleted && s.name === storeQueryParam);
+        }
+        return store.find(s => s.status !== StoreStatus.deleted);
+    }
+
+    protected isNotAuthorized(observer) {
+        this.redirectToLogin();
+        this.windowRef.nativeWindow.localStorage.removeItem('Authorization');
+        observer.next(false);
+        observer.complete();
     }
 }
