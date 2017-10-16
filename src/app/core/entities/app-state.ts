7b1702ce74ec260b22d74933d63394452870ef7d
maksymzav <maksym.zavorotynskyi@gmail.com>
2017-10-16 15:48:23 +0300

    Remove unused channels property from the app state.

--- src/app/core/entities/app-state.ts
+++ src/app/core/entities/app-state.ts
@@ -1,11 +1,10 @@
 import { AggregatedUserInfo } from './aggregated-user-info';
 import { Store } from './store';
-import { StoreChannelResponse } from './store-channel-response';
 import { Statistics } from './statistics';
+import { TimelineEventFormatted } from './timeline-event-formatted';
 
 export interface AppState {
     userInfo: AggregatedUserInfo;
     currentStore: Store;
-    channels: StoreChannelResponse
     storeStatistics: Statistics
 }
