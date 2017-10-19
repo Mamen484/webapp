519d5be84195989238481f3b18d5e81d68bc18a5
maksymzav <maksym.zavorotynskyi@gmail.com>
2017-10-19 19:18:49 +0300

    Fix proper locale redirection to use current path.

--- src/app/core/guards/check-proper-locale.guard.spec.ts
+++ src/app/core/guards/check-proper-locale.guard.spec.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs/Observable';
 import { LocaleIdService } from '../services/locale-id.service';
 import { environment } from '../../../environments/environment';
 import { WindowRefService } from '../services/window-ref.service';
+import { Location } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CheckProperLocaleGuard', () => {
 
@@ -12,11 +14,13 @@ describe('CheckProperLocaleGuard', () => {
     describe('the same locale in the /me response and the localization of the project', () => {
         beforeEach(() => {
             TestBed.configureTestingModule({
+                imports: [RouterTestingModule.withRoutes([])],
                 providers: [
                     CheckProperLocaleGuard,
                     {provide: UserService, useValue: {fetchAggregatedInfo: () => Observable.of({language: 'it_IT'})}},
                     {provide: LocaleIdService, useValue: {localeId: 'it'}},
-                    {provide: WindowRefService, useValue: {}}
+                    {provide: WindowRefService, useValue: {}},
+                    Location,
                 ]
             });
         });
@@ -32,11 +36,13 @@ describe('CheckProperLocaleGuard', () => {
     describe('different locales in the /me response and the localization of the project', () => {
         beforeEach(() => {
             TestBed.configureTestingModule({
+                imports: [RouterTestingModule.withRoutes([])],
                 providers: [
                     CheckProperLocaleGuard,
                     {provide: UserService, useValue: {fetchAggregatedInfo: () => Observable.of({language: 'en_US'})}},
                     {provide: LocaleIdService, useValue: {localeId: 'it'}},
-                    {provide: WindowRefService, useValue: {nativeWindow: {location: {href: ''}}}}
+                    {provide: WindowRefService, useValue: {nativeWindow: {location: {href: ''}}}},
+                    Location,
                 ]
             });
         });
