bc5ab25c69bb73966d9b067005987debfca56562
maksymzav <maksym.zavorotynskyi@gmail.com>
2017-11-02 12:28:26 +0200

    Revert "Revert "Change autopilot run logic.""
    This reverts commit c53543c2a39e701847d9a328b381eba75b90ffb3.

--- src/app/base/base.component.ts
+++ src/app/base/base.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../core/entities/app-state';
+import { environment } from '../../environments/environment'
 
 declare const Autopilot;
 
@@ -11,12 +12,21 @@ declare const Autopilot;
 })
 export class BaseComponent {
 
-    constructor(protected appStore: Store<AppState>) {
+    constructor(protected appStore: Store<AppState>, @Inject('windowObject') protected window: any) {
         this.appStore.select('userInfo')
             .combineLatest(this.appStore.select('currentStore'))
             .subscribe(([userInfo, currentStore]) => {
-                Autopilot.run('associate',
-                    {_simpleAssociate: true, Email: userInfo.email, FirstName: currentStore.name});
+                if (userInfo.login === currentStore.name) {
+                    this.window.Autopilot.run('associate',
+                        {_simpleAssociate: true, Email: userInfo.email, FirstName: currentStore.name});
+                } else {
+                    this.window.Autopilot.run('associate',
+                        {
+                            _simpleAssociate: true,
+                            Email: environment.DEFAULT_AUTOPILOT_EMAIL,
+                            FirstName: environment.DEFAULT_AUTOPILOT_STORENAME,
+                        });
+                }
             })
     }
 }
