fe6f89a0812486ef66a0144adf74ea544688c6dd
maksymzav <maksym.zavorotynskyi@gmail.com>
2017-12-20 00:50:39 +0200

    SFWEB-87 Fix base component test.

--- src/app/base/base.component.spec.ts
+++ src/app/base/base.component.spec.ts
@@ -1,21 +1,25 @@
 import { Observable } from 'rxjs/Observable';
 import { BaseComponent } from './base.component';
 import { environment } from '../../environments/environment';
+import { StoreService } from '../core/services/store.service';
 
 describe('BaseComponent', () => {
 
     let store, window = {Autopilot: <any>{}};
+    let storeService: jasmine.SpyObj<StoreService>;
     beforeEach(() => {
         store = jasmine.createSpyObj('Store', ['select']);
         window.Autopilot = jasmine.createSpyObj('Autopilot', ['run']);
+        storeService = jasmine.createSpyObj('StoreService', ['getStoreChannels']);
     });
 
     it ('should run autopilot associate with store\'s name and user\'s email', () => {
         store.select.and.returnValues(
             Observable.of({login: 'login1', email: 'email1'}),
-            Observable.of({name: 'login1'})
+            Observable.of({name: 'login1'}),
+            Observable.empty(),
         );
-        let component = new BaseComponent(store, <any>{nativeWindow: window});
+        let component = new BaseComponent(store, <any>{nativeWindow: window}, storeService);
         expect(window.Autopilot.run.calls.mostRecent().args[0]).toEqual('associate');
         expect(window.Autopilot.run.calls.mostRecent().args[1]._simpleAssociate).toEqual(true);
         expect(window.Autopilot.run.calls.mostRecent().args[1].Email).toEqual('email1');
@@ -25,9 +29,10 @@ describe('BaseComponent', () => {
     it ('should run autopilot associate with store\'s name and user\'s email', () => {
         store.select.and.returnValues(
             Observable.of({login: 'login1', email: 'email1'}),
-            Observable.of({name: 'login2'})
+            Observable.of({name: 'login2'}),
+            Observable.empty(),
         );
-        let component = new BaseComponent(store, <any>{nativeWindow: window});
+        let component = new BaseComponent(store, <any>{nativeWindow: window}, storeService);
         expect(window.Autopilot.run.calls.mostRecent().args[0]).toEqual('associate');
         expect(window.Autopilot.run.calls.mostRecent().args[1]._simpleAssociate).toEqual(true);
         expect(window.Autopilot.run.calls.mostRecent().args[1].Email).toEqual(environment.DEFAULT_AUTOPILOT_EMAIL);
