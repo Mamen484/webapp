09c3f5c3d1b2d6642d1fd6b4d7ea62107f444a73
maksymzav <maksym.zavorotynskyi@gmail.com>
2018-01-16 16:52:27 +0200

    Use UTC dates in orders-filter-dialog spec.

--- src/app/orders/orders-filter-dialog/orders-filter-dialog.component.spec.ts
+++ src/app/orders/orders-filter-dialog/orders-filter-dialog.component.spec.ts
@@ -32,7 +32,7 @@ describe('OrdersFilterDialogComponent', () => {
             schemas: [NO_ERRORS_SCHEMA]
         });
 
-        let baseTime = new Date(2017, 5, 14, 2, 22, 41);
+        let baseTime = new Date(Date.UTC(2017, 5, 14, 2, 22, 41));
         jasmine.clock().mockDate(baseTime);
     }));
 
@@ -87,31 +87,31 @@ describe('OrdersFilterDialogComponent', () => {
 
         it('should assign a `since` property to current day subtract 7 days when a `week` is passed to changeDate', () => {
             component.changeDate('week');
-            expect(component.filter.since.getTime()).toEqual(1496791361000); // 2017-06-07 2:22:41
+            expect(component.filter.since.getTime()).toEqual(1496802161000); // 2017-06-07 2:22:41
         });
 
         it('should assign a `since` property to current day subtract 30 days when a `month` is passed to changeDate', () => {
             component.changeDate('month');
-            expect(component.filter.since.getTime()).toEqual(1494804161000); // 2017-05-15 2:22:41
+            expect(component.filter.since.getTime()).toEqual(1494814961000); // 2017-05-15 2:22:41
         });
 
         it('should remove an `until` property on changeDate call with `today` passing in', () => {
             component.filter.until = new Date();
-            expect(component.filter.until.getTime()).toEqual(1497396161000);
+            expect(component.filter.until.getTime()).toEqual(1497406961000);
             component.changeDate('today');
             expect(component.filter.until).not.toBeDefined();
         });
 
         it('should remove an `until` property on changeDate call with `week` passing in', () => {
             component.filter.until = new Date();
-            expect(component.filter.until.getTime()).toEqual(1497396161000);
+            expect(component.filter.until.getTime()).toEqual(1497406961000);
             component.changeDate('week');
             expect(component.filter.until).not.toBeDefined();
         });
 
         it('should remove an `until` property on changeDate call with `month` passing in', () => {
             component.filter.until = new Date();
-            expect(component.filter.until.getTime()).toEqual(1497396161000);
+            expect(component.filter.until.getTime()).toEqual(1497406961000);
             component.changeDate('month');
             expect(component.filter.until).not.toBeDefined();
         });
@@ -119,8 +119,8 @@ describe('OrdersFilterDialogComponent', () => {
         it('should remove `since` and `until` properties on changeDate call with `anytime` passing in', () => {
             component.filter.until = new Date();
             component.filter.since = new Date();
-            expect(component.filter.until.getTime()).toEqual(1497396161000);
-            expect(component.filter.since.getTime()).toEqual(1497396161000);
+            expect(component.filter.until.getTime()).toEqual(1497406961000);
+            expect(component.filter.since.getTime()).toEqual(1497406961000);
             component.changeDate('anytime');
             expect(component.filter.until).not.toBeDefined();
             expect(component.filter.since).not.toBeDefined();
@@ -140,14 +140,14 @@ describe('OrdersFilterDialogComponent', () => {
 
         it('should assign a `since` filter property on setSince() call ', () => {
             expect(component.filter.since).not.toBeDefined();
-            component.setSince(1496791361000);
-            expect(component.filter.since.getTime()).toEqual(1496791361000);
+            component.setSince(1496802161000);
+            expect(component.filter.since.getTime()).toEqual(1496802161000);
         });
 
         it('should assign an `until` filter property on setUntil() call ', () => {
             expect(component.filter.until).not.toBeDefined();
-            component.setUntil(1496791361000);
-            expect(component.filter.until.getTime()).toEqual(1496791361000);
+            component.setUntil(1496802161000);
+            expect(component.filter.until.getTime()).toEqual(1496802161000);
         });
     });
 
