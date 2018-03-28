652d545eea4b1070b224dab6ffb28c028905b8c2
maksymzav <maksym.zavorotynskyi@gmail.com>
2018-03-28 17:16:10 +0200

    Add a logout button to the admin interface.

--- src/app/admin-dashboard/admin-menu/admin-menu.component.ts
+++ src/app/admin-dashboard/admin-menu/admin-menu.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from '../../core/services/local-storage.service';
+import { WindowRefService } from '../../core/services/window-ref.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
     selector: 'sf-admin-menu',
@@ -6,4 +9,13 @@ import { Component } from '@angular/core';
     styleUrls: ['./admin-menu.component.scss']
 })
 export class AdminMenuComponent {
+
+    constructor(protected windowRef: WindowRefService,
+                protected localStorage: LocalStorageService) {
+    }
+
+    logout() {
+        this.localStorage.removeItem('Authorization');
+        this.windowRef.nativeWindow.location.href = `${environment.APP_URL}/index/logout`;
+    }
 }
