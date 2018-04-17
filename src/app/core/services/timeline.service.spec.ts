d648badec24c0ccea5e634661073defa960ebe4d
maksymzav <maksym.zavorotynskyi@gmail.com>
2018-04-17 20:38:37 +0300

    Remove deprecated spec.

--- src/app/core/services/timeline.service.spec.ts
+++ src/app/core/services/timeline.service.spec.ts
@@ -56,24 +56,6 @@ describe('TimelineService', () => {
 
     });
 
-    it('should create an array of distinct updates: only the one last import and one last export of each channel', () => {
-        httpClient.get.and.returnValue(Observable.of(data2));
-
-        service.getEventUpdates(307).subscribe((updates: Timeline<TimelineUpdate>) => {
-            let upd = updates._embedded.timeline;
-            // feed.export - Amazon
-            expect(upd[0].id).toEqual('5a8be8cf14f698306a067839');
-            // feed.export - CDiscount
-            expect(upd[1].id).toEqual('5a8be8c114f698306a067837');
-            // feed.import
-            expect(upd[2].id).toEqual('5a8be8bb6ec8c32ec02cf4b9');
-            // feed.export - Fnac
-            expect(upd[3].id).toEqual('5a8be75214f698306a06780a');
-
-            expect(upd.length).toEqual(4);
-        });
-    });
-
     it('should pass proper params on getEvents call', () => {
         service.getEvents(114).subscribe();
 
