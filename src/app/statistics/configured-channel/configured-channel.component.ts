15ce7f2ffff1c248430eddec986e2418737be096
maksymzav <maksym.zavorotynskyi@gmail.com>
2018-06-18 15:48:51 +0300

    SFWEB-212 Handle unavailability of channel statistics.

--- src/app/statistics/configured-channel/configured-channel.component.ts
+++ src/app/statistics/configured-channel/configured-channel.component.ts
@@ -1,20 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { StoreChannel } from '../../core/entities/store-channel';
-import { WindowRefService } from '../../core/services/window-ref.service';
 import { Channel } from '../../core/entities/channel';
 
 @Component({
     selector: 'sf-configured-channel',
     templateUrl: './configured-channel.component.html',
-    styleUrls: ['./configured-channel.component.scss']
+    styleUrls: ['./configured-channel.component.scss'],
 })
 export class ConfiguredChannelComponent {
 
     @Input() channel: StoreChannel;
     @Input() hasStatisticsPermission = false;
-
-    constructor(protected windowRef: WindowRefService) {
-    }
 
     goToChannelLink(channel: Channel) {
         return channel.type === 'marketplace'
@@ -22,4 +18,8 @@ export class ConfiguredChannelComponent {
             : `/${channel.type}/manage/${channel.name}`;
     }
 
+    // @TODO: improve performance of change detection
+    statisticsExistsFor(property) {
+        return Boolean(this.channel.statistics) && typeof this.channel.statistics[property] === 'number';
+    }
 }
