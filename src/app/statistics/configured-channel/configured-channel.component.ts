e00d32320507d1ffc5d10458949a1d79aea9e49d
maksymzav <maksym.zavorotynskyi@gmail.com>
2018-09-03 14:23:07 +0200

    SFWEB-222 Redesign channels.

--- src/app/statistics/configured-channel/configured-channel.component.ts
+++ src/app/statistics/configured-channel/configured-channel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { StoreChannel } from '../../core/entities/store-channel';
 import { Channel } from '../../core/entities/channel';
 
@@ -7,10 +7,16 @@ import { Channel } from '../../core/entities/channel';
     templateUrl: './configured-channel.component.html',
     styleUrls: ['./configured-channel.component.scss'],
 })
-export class ConfiguredChannelComponent {
+export class ConfiguredChannelComponent implements OnInit {
 
     @Input() channel: StoreChannel;
     @Input() hasStatisticsPermission = false;
+
+    channelsOnline: number;
+
+    ngOnInit() {
+        this.initializeOnline();
+    }
 
     goToChannelLink(channel: Channel) {
         return channel.type === 'marketplace'
@@ -21,5 +27,18 @@ export class ConfiguredChannelComponent {
     // @TODO: improve performance of change detection
     statisticsExistsFor(property) {
         return Boolean(this.channel.statistics) && typeof this.channel.statistics[property] === 'number';
+    }
+
+    protected initializeOnline() {
+        if (!this.statisticsExistsFor('exported')
+            || !this.statisticsExistsFor('selected')
+            || this.channel.statistics.selected === 0) {
+            return;
+        }
+
+        this.channelsOnline = Math.round(
+            Number(this.channel.statistics.exported)
+            / Number(this.channel.statistics.selected)
+            * 100);
     }
 }
