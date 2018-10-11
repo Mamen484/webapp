ac96120923b2640ee109298bf00506b5dd7571aa
maksymzav <maksym.zavorotynskyi@gmail.com>
2018-10-11 09:25:05 +0300

    Remove a token from an address.

--- src/app/core/guards/login-by-token.guard.ts
+++ src/app/core/guards/login-by-token.guard.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { LocalStorageService } from '../services/local-storage.service';
+import { Location } from '@angular/common';
 
 @Injectable()
 export class LoginByTokenGuard implements CanActivate {
 
-    constructor(protected localStorage: LocalStorageService) {
+    constructor(protected localStorage: LocalStorageService, protected location: Location) {
     }
 
     canActivate(next: ActivatedRouteSnapshot): true {
         if (next.queryParamMap.has('token')) {
             this.localStorage.setItem('Authorization', 'Bearer ' + next.queryParams.token);
+            // remove a token from an url to prevent unneeded sharing the token by coping an url from a browser address bar
+            this.location.replaceState(this.location.path().replace(/token=[a-zA-Z0-9]*&?/, ''));
         }
 
         return true;
