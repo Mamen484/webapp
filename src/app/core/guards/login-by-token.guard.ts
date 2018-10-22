33a9ed2533600ccfc9205f7ff4610881c25e1702
maksymzav <maksym.zavorotynskyi@gmail.com>
2018-10-22 19:50:54 +0300

    Revert "Move replacing state to the later stage."
    This reverts commit d37069ac7507713027cefc57973ddaacdc075c01.

--- src/app/core/guards/login-by-token.guard.ts
+++ src/app/core/guards/login-by-token.guard.ts
@@ -1,20 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate } from '@angular/router';
 import { LocalStorageService } from '../services/local-storage.service';
-import { Location } from '@angular/common';
-
-const TOKEN_IN_URL = /token=[a-zA-Z0-9]*&?/;
 
 @Injectable()
 export class LoginByTokenGuard implements CanActivate {
 
-    constructor(protected localStorage: LocalStorageService, protected location: Location) {
+    constructor(protected localStorage: LocalStorageService) {
     }
 
     canActivate(next: ActivatedRouteSnapshot): true {
         if (next.queryParamMap.has('token')) {
             this.localStorage.setItem('Authorization', 'Bearer ' + next.queryParams.token);
-            this.location.replaceState(this.location.path().replace(TOKEN_IN_URL, ''));
         }
 
         return true;
