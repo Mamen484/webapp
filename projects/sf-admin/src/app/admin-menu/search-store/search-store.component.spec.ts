8e40bfceb612b231e740f17a6f6bbc07adff0e7c
maksymzav <maksym.zavorotynskyi@gmail.com>
2018-11-26 19:05:16 +0200

    SFWEB-262 Change search-store code to work with mat-autocomplete.

--- projects/sf-admin/src/app/admin-menu/search-store/search-store.component.spec.ts
+++ projects/sf-admin/src/app/admin-menu/search-store/search-store.component.spec.ts
@@ -1,8 +1,9 @@
-import { async, ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SearchStoreComponent } from './search-store.component';
 import { Directive, ElementRef, NO_ERRORS_SCHEMA } from '@angular/core';
 import { SflWindowRefService } from 'sfl-shared/services';
 import { environment } from '../../../environments/environment';
+import { MatAutocompleteModule } from '@angular/material';
 
 @Directive({selector: '[sfaSearchStore]'})
 export class SearchStoreMockDirective {
@@ -19,6 +20,7 @@ describe('SearchStoreComponent', () => {
         elementRef = {};
         windowRef = {nativeWindow: jasmine.createSpyObj('Window', ['open'])};
         TestBed.configureTestingModule({
+            imports: [MatAutocompleteModule],
             declarations: [SearchStoreComponent, SearchStoreMockDirective],
             schemas: [NO_ERRORS_SCHEMA],
             providers: [
@@ -34,15 +36,31 @@ describe('SearchStoreComponent', () => {
         fixture.detectChanges();
     });
 
-    it('should go to ?store={storeId} on selectStore call', fakeAsync(() => {
+    it('should go to ?store={storeId} on selectStore call', () => {
         component.selectStore(295);
         expect(windowRef.nativeWindow.open).toHaveBeenCalledWith(`${environment.WEBAPP_URL}?store=295`);
-    }));
+    });
 
-    it('should set processing to true on selectStore call', fakeAsync(() => {
+    it('should set processing to true on selectStore call', () => {
         component.processing = true;
         component.selectStore(295);
         expect(component.processing).toEqual(false);
-    }));
+    });
+
+    it('should display a store name in the input when a store is selected', () => {
+        expect(component.displayFn(<any>{id: 21, name: 'some name'})).toBe('some name');
+    });
+
+    it('should return undefined if displayFn is called without a store', () => {
+        expect(component.displayFn(undefined)).not.toBeDefined();
+    });
+
+    it('should reset search on clearSearch call', () => {
+        component.searchResults = [];
+        component.searchControl.setValue('some value');
+        component.clearSearch();
+        expect(component.searchResults).not.toBeDefined();
+        expect(component.searchControl.value).toBe(null);
+    });
 
 });
--- projects/sf-admin/src/app/admin-menu/search-store/search-store.component.ts
+++ projects/sf-admin/src/app/admin-menu/search-store/search-store.component.ts
@@ -1,70 +1,37 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { SflWindowRefService } from 'sfl-shared/services';
 import { environment } from '../../../environments/environment';
+import { Store } from 'sfl-shared/entities';
 
 @Component({
     selector: 'sfa-search-store',
     templateUrl: './search-store.component.html',
     styleUrls: ['./search-store.component.scss'],
 })
-export class SearchStoreComponent implements OnInit, AfterViewInit {
+export class SearchStoreComponent {
 
     @Output() focus = new EventEmitter();
     @Output() blur = new EventEmitter();
 
     searchControl = new FormControl();
     searchResults: { name: string, id: number }[];
-    protected _showSearchResult = false;
-    get showSearchResult() {
-        return this._showSearchResult;
-    }
-
-    set showSearchResult(value) {
-        if (value === this._showSearchResult) {
-            return;
-        }
-        this._showSearchResult = value;
-        if (value) {
-            this.focus.emit();
-        } else {
-            this.blur.emit();
-        }
-        setTimeout(() => {
-            // schedule changing lookup position and width after container changes size
-            this.setSearchResultsPosition();
-        });
-    }
 
     processing = false;
-
-    @HostListener('window:resize')
-    setSearchResultsPosition() {
-        let width = this.elementRef.nativeElement.querySelector('.toolbar-search-container').getBoundingClientRect().width;
-        this.elementRef.nativeElement.querySelector('.search-results').style.width = `${width}px`;
-
-        let leftPosition = this.elementRef.nativeElement.querySelector('.toolbar-search-container').getBoundingClientRect().left;
-        this.elementRef.nativeElement.querySelector('.search-results').style.left = `${leftPosition}px`;
-    }
 
     constructor(protected elementRef: ElementRef,
                 protected windowRef: SflWindowRefService) {
     }
 
-    ngOnInit() {
-    }
-
-    ngAfterViewInit() {
-        this.setSearchResultsPosition()
-    }
-
     clearSearch() {
-
-        this.showSearchResult = false;
         this.searchControl.reset();
         this.searchResults = undefined;
 
         return false;
+    }
+
+    displayFn(store: Store) {
+        return store ? store.name : undefined;
     }
 
     selectStore(storeId) {
