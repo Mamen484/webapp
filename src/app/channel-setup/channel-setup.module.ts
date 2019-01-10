f0611c092e8357b624a1bfd7465b7c85f3e2d375
maksymzav <maksym.zavorotynskyi@gmail.com>
2019-01-10 09:32:14 +0200

    SFWEB-334 SFWEB-335 Remove unused dialogs.

--- src/app/channel-setup/channel-setup.module.ts
+++ src/app/channel-setup/channel-setup.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChannelSetupRoutingModule } from './channel-setup-routing.module';
-import { AmazonAccountDialogComponent } from './amazon-account-dialog/amazon-account-dialog.component';
 import { CategoriesConfigurationComponent } from './categories-configuration/categories-configuration.component';
 import { SharedModule } from '../shared/shared.module';
-import { WelcomeInstructionsComponent } from './welcome-instructions/welcome-instructions.component';
 import { FilterDialogComponent } from './filter-dialog/filter-dialog.component';
 
 @NgModule({
     declarations: [
-        AmazonAccountDialogComponent,
         CategoriesConfigurationComponent,
         FilterDialogComponent,
-        WelcomeInstructionsComponent,
     ],
     imports: [
         ChannelSetupRoutingModule,
@@ -22,9 +18,7 @@ import { FilterDialogComponent } from './filter-dialog/filter-dialog.component';
 
     ],
     entryComponents: [
-        AmazonAccountDialogComponent,
         FilterDialogComponent,
-        WelcomeInstructionsComponent,
     ],
 })
 export class ChannelSetupModule {
