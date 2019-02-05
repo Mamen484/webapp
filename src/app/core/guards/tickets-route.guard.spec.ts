2c4dc02cafcbdd457ef1ab55814df451170d79a7
maksymzav <maksym.zavorotynskyi@gmail.com>
2019-02-05 22:29:10 +0200

    SFWEB-350 Add a link to a tickets page.

---
+++ src/app/core/guards/tickets-route.guard.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
+import { AppState } from '../entities/app-state';
+import { TicketsRouteGuard } from './tickets-route.guard';
+
+describe('TicketsRouteGuard', () => {
+  let store: jasmine.SpyObj<Store<AppState>>;
+  beforeEach(() => {
+    store = jasmine.createSpyObj(['dispatch']);
+    TestBed.configureTestingModule({
+      providers: [
+          TicketsRouteGuard,
+          {provide: Store, useValue: store}
+      ]
+    });
+  });
+
+  it('should write `tickets` as the currentRoute value and return true', inject([TicketsRouteGuard], (guard: TicketsRouteGuard) => {
+    expect(guard.canActivate()).toEqual(true);
+    expect(store.dispatch).toHaveBeenCalledWith({type: 'SET_ROUTE', routeName: 'tickets'});
+  }));
+
+});
--- src/app/tickets/tickets-routing.module.ts
+++ src/app/tickets/tickets-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TicketsListComponent } from './tickets-list/tickets-list.component';
+import { TicketsRouteGuard } from '../core/guards/tickets-route.guard';
 
 const routes: Routes = [
-    {path: '', component: TicketsListComponent}
+    {path: '', component: TicketsListComponent, canActivate: [TicketsRouteGuard]}
 ];
 
 @NgModule({
