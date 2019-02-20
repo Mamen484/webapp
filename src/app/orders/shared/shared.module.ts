411777856d38b268e1ca355060b8b9c52b56fd3b
maksymzav <maksym.zavorotynskyi@gmail.com>
2019-02-20 19:04:35 +0200

    SFWEB-373 Implement new behavior for orders left sidenav.

--- src/app/orders/shared/shared.module.ts
+++ src/app/orders/shared/shared.module.ts
@@ -5,23 +5,28 @@ import { ConfirmCancellationDialogComponent } from './confirm-cancellation-dialo
 import { TagChipComponent } from './tag-chip/tag-chip.component';
 import { OrdersLeftNavComponent } from './orders-left-nav/orders-left-nav.component';
 import { SflSidebarModule } from 'sfl-shared/sidebar';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { ExpandableComponent } from './expandable/expandable.component';
 
 @NgModule({
     imports: [
         CommonModule,
         GlobalSharedModule,
         SflSidebarModule,
+        CdkAccordionModule,
     ],
     declarations: [
         ConfirmCancellationDialogComponent,
         TagChipComponent,
         OrdersLeftNavComponent,
+        ExpandableComponent,
     ],
     exports: [
         GlobalSharedModule,
         ConfirmCancellationDialogComponent,
         TagChipComponent,
         OrdersLeftNavComponent,
+        ExpandableComponent,
     ],
     entryComponents: [
         ConfirmCancellationDialogComponent,
