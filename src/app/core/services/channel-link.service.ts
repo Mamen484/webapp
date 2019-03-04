8de15525f14376330c94b96ac23d2119443776f4
maksymzav <maksym.zavorotynskyi@gmail.com>
2019-03-04 13:39:14 +0200

    SFWEB-346 Always create feed for not opened channels, never for opened.

--- src/app/core/services/channel-link.service.ts
+++ src/app/core/services/channel-link.service.ts
@@ -6,7 +6,7 @@ import { ChannelLinkPipe } from '../../shared/channel-link/channel-link.pipe';
 import { FeedService } from './feed.service';
 import { ChannelService } from './channel.service';
 import { CategoryMapping } from '../../channel-setup/category-mapping';
-import { SflWindowRefService } from 'sfl-shared/services';
+import { SflLocaleIdService, SflWindowRefService } from 'sfl-shared/services';
 import { Router } from '@angular/router';
 import { Channel } from 'sfl-shared/entities';
 import { LegacyLinkService } from './legacy-link.service';
@@ -20,38 +20,42 @@ export class ChannelLinkService {
                 protected channelService: ChannelService,
                 protected windowRefService: SflWindowRefService,
                 protected legacyLinkService: LegacyLinkService,
-                protected router: Router) {
+                protected router: Router,
+                protected localeIdService: SflLocaleIdService) {
     }
 
     navigateToChannel(channel: Channel) {
-        zip(
-            this.feedService.fetchFeedCollection(channel.id).pipe(
-                flatMap(feedCollection => {
-                    if (!feedCollection.total) {
-                        return this.feedService.create(channel.id)
-                            .pipe(flatMap(() => this.feedService.fetchFeedCollection(channel.id)))
-                    }
-                    return of(feedCollection);
-                })
-            ),
+        this.decideToSkipSetup(channel).subscribe(skipSetup => {
+            if (skipSetup) {
+                this.goToChannel(ChannelLinkPipe.getChannelLink(channel));
+            } else {
+                this.goToChannelSetup(channel.id);
+            }
+        });
+    }
+
+    protected decideToSkipSetup(channel) {
+        return zip(
+            this.getFeedCollection(channel),
             this.channelService.getChannelCategories(channel.id, {limit: '1'})
         ).pipe(
             map(([feed, categoriesPage]) => ([feed._embedded.feed[0], categoriesPage._embedded.category.length > 0])),
             flatMap(([feed, channelHasCategories]: [Feed, boolean]) =>
                 channelHasCategories ? this.hasConfiguredCategories(feed.id) : of(true)
-            ))
-            .subscribe(skipSetup => {
-                if (skipSetup) {
-                    this.goToChannel(ChannelLinkPipe.getChannelLink(channel));
-                } else {
-                    this.goToChannelSetup(channel.id);
-                }
-            });
+            ));
     }
 
     protected hasConfiguredCategories(feedId): Observable<boolean> {
         return this.feedService.fetchCategoryCollection(feedId, {mapping: CategoryMapping.Mapped})
             .pipe(map(response => Boolean(response._embedded.category.length)));
+    }
+
+    protected getFeedCollection(channel) {
+        if (channel.installed) {
+            return this.feedService.fetchFeedCollection(channel.id);
+        }
+        return this.feedService.create(channel.id)
+            .pipe(flatMap(() => this.feedService.fetchFeedCollection(channel.id)))
     }
 
     protected goToChannel(channelLink) {
