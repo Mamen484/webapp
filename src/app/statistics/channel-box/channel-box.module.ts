4926044b168a15d4e2f4339b8f566631b4f8cfcf
maksymzav <maksym.zavorotynskyi@gmail.com>
2019-09-25 09:51:20 +0300

    SFWEB-485 Change channel box view.

--- src/app/statistics/channel-box/channel-box.module.ts
+++ src/app/statistics/channel-box/channel-box.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChannelTurnoverComponent } from './channel-turnover/channel-turnover.component';
-import { ChannelOnlineComponent } from './channel-online/channel-online.component';
-import { StatsUnavailableComponent } from './stats-unavailable/stats-unavailable.component';
-import { ChannelBoxComponent } from './channel-box.component';
 import { SharedModule } from '../../shared/shared.module';
 import { ConfiguredChannelComponent } from './configured-channel/configured-channel.component';
 import { SuggestedChannelComponent } from './suggested-channel/suggested-channel.component';
@@ -15,10 +11,6 @@ import { SuggestedChannelComponent } from './suggested-channel/suggested-channel
     ],
     declarations: [
         ConfiguredChannelComponent,
-        ChannelBoxComponent,
-        ChannelTurnoverComponent,
-        ChannelOnlineComponent,
-        StatsUnavailableComponent,
         SuggestedChannelComponent
     ],
     exports: [
