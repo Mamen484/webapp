20c3562fe579c98714bb6c0f336555f582ec4081
maksymzav <maksym.zavorotynskyi@gmail.com>
2019-11-19 17:27:51 +0200

    SFWEB-523 Change squarespace error page. (#244)

--- projects/sfl-shared/error-pages/src/error-page/error-page.component.ts
+++ projects/sfl-shared/error-pages/src/error-page/error-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'sfl-error-page',
@@ -6,4 +6,5 @@ import { Component } from '@angular/core';
     templateUrl: 'error-page.component.html',
 })
 export class SflErrorPageComponent {
+    @Input() showLink = true;
 }
--- src/app/squarespace/error/error.component.ts
+++ src/app/squarespace/error/error.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    template: `<sfl-error-page i18n>An error occurred</sfl-error-page>`,
+    template: `<sfl-error-page [showLink]="false">
+        <div i18n>Failed to connect to Squarespace</div>
+        <button mat-button i18n routerLink="/squarespace/authentify" color="accent">Try again</button>
+    </sfl-error-page>`,
     styles: [],
 })
 export class ErrorComponent implements OnInit {
