9bf4fe3c46ab6d1a1659e051ca955ed4a546fd38
maksymzav <maksym.zavorotynskyi@gmail.com>
2020-02-10 15:26:01 +0200

    Fix running tests on webapp. (#262)

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,5 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
-
-if (!process.env.CHROME_BIN){
-    process.env.CHROME_BIN = '/usr/bin/chromium-browser';
-}
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
     config.set({
